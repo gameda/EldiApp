fa5c0e4d625edbe827cea5eaec669050ed791aa9
Daniel <danlaster@hotmail.com>
2016-11-11 13:29:39 -0600

    -Se agrega clase LoadData para marndar conectarse con el servidor

---
+++ app/src/main/java/itesm/eldyapp/LoadData.java
@@ -0,0 +1,97 @@
+package itesm.eldyapp;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Daniel on 10/11/16.
+ */
+
+public class LoadData extends AsyncTask<String, Void, JSONObject> {
+
+    private ProgressDialog progressDialog;
+    private Context context;
+
+
+    public LoadData() {
+
+    }
+
+    @Override
+    protected void onPreExecute(){
+        //Lo que muestra miestras carga
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        super.onPreExecute();
+
+    }
+    @Override
+    protected JSONObject doInBackground(String... params) {
+        String sData = params[0];
+        String sText = "";
+        BufferedReader bfReader=null;
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL("10.2.145.43:7777/compile?program=algo");
+
+            // Send POST data request
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( sData );
+            wr.flush();
+            wr.close ();
+
+            // Get the server response
+
+            bfReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line="";
+
+            // Read Server Response
+            while((line = bfReader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            sText = sb.toString();
+        } catch(Exception ex) {
+
+        }
+        finally {
+            try {
+
+                bfReader.close();
+            }
+
+            catch(Exception e) {}
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject jsonObject){
+
+
+    }
+}
--- app/src/main/java/itesm/eldyapp/ReadValue.java
+++ app/src/main/java/itesm/eldyapp/ReadValue.java
@@ -10,10 +10,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLEncoder;
 
-public class ReadValue extends AppCompatActivity {
+public class ReadValue extends AppCompatActivity  {
 
     TextView varTV;
     String [] sValor = null;
@@ -38,7 +44,6 @@ public class ReadValue extends AppCompatActivity {
 
 
 
-
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -55,7 +60,9 @@ public class ReadValue extends AppCompatActivity {
 
                     }
                     sJson += variablesJson(sValor, iVar);
-                    String url = "10.2.145.43:7777/compile?program=algo" + queryEncode(sJson);
+                    //String url = "10.2.145.43:7777/compile?program=algo" + queryEncode(sJson);
+                    connection(queryEncode(sJson));
+
 
 
 
@@ -108,6 +115,63 @@ public class ReadValue extends AppCompatActivity {
 
         return sQuery;
     }
+
+    public void connection(String sData){
+
+        String sText = "";
+        BufferedReader bfReader=null;
+        // Send data
+        try
+        {
+
+            // Defined URL  where to send data
+            URL url = new URL("10.2.145.43:7777/compile?program=algo");
+
+            // Send POST data request
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( sData );
+            wr.flush();
+            wr.close ();
+
+            // Get the server response
+
+            bfReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+
+            // Read Server Response
+            while((line = bfReader.readLine()) != null) {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+
+            sText = sb.toString();
+        } catch(Exception ex) {
+
+        }
+        finally {
+            try {
+
+                bfReader.close();
+            }
+
+            catch(Exception e) {}
+        }
+
+        // Show response on activity
+        //content.setText( sText  );
+        //http://www.xyzws.com/javafaq/how-to-use-httpurlconnection-post-data-to-web-server/139
+
+
+    }
+
 
 
 
